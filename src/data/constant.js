e1256f1395f5de0124edb7ad45784578cd80ee5c
cricket lover <sami76748@gmail.com>
2024-04-25 01:27:45 -0700

    bjbjbjb

--- src/data/constant.js
+++ src/data/constant.js
@@ -10,7 +10,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/SamiAhmed788",
     resume:
-      "file:///C:/Users/User/Downloads/justo.%20Ut%20aliquam%20augue..pdf",
+      "https://drive.google.com/file/d/1et82Q0bfDZ8WWmAHOAmYnmVRJc-Ry9LT/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/sami-ahmed-a21aa1301?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     twitter: "https://twitter.com",
     // insta: "https://www.instagram.com/rishav_chanda/",
